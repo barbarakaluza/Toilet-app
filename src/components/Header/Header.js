4711f87131b182722586718ff19d1d5ac60be4d0
Barbara <kaluzabarbara26@gmail.com>
2024-02-13 18:59:07 +0100

    creating hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-import Nav from '../Nav/Nav';
-import NavLink from '../Nav/NavLink';
+// import Nav from '../Nav/Nav';
+// import NavLink from '../Nav/NavLink';
 
 
 const Header = () => {
-  return (
-    <header className="header">
-        <Nav className="nav">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/">Articles</NavLink>
-                <NavLink to="/">Register</NavLink>
-                <NavLink to="/">Contact</NavLink>
-        </Nav>
-
-
-      <button className="toggle-button">
-        <FontAwesomeIcon icon={faBars} className='icon-bar'/>
-      </button>
-    </header>
-    
-  );
-};
+    const [isNavOpen, setIsNavOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsNavOpen(!isNavOpen);
+    };
+  
+    return (
+      <header className="header">
+        <nav className={`nav ${isNavOpen ? 'open' : ''}`}>
+          <ul className='nav-ul'>
+            <li className='nav-li'><a className="nav-link" href="/">Home</a></li>
+            <li className='nav-li'><a className="nav-link" href="/">Articles</a></li>
+            <li className='nav-li'><a className="nav-link"href="/">Register</a></li>
+            <li className='nav-li'><a className="nav-link"href="/">Contact</a></li>
+          </ul>
+        </nav>
+  
+        <button className="toggle-button" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={faBars} className='icon-bar'/>
+        </button>
+      </header>
+    );
+  };
+  
 
 export default Header;
 
