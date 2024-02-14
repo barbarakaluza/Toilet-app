5e13f5688dfbbcc503307b3e24507012c28f9f3c
Barbara <kaluzabarbara26@gmail.com>
2024-02-14 01:12:43 +0100

    add opinion page

--- src/components/Opinions/OpinionForm.js
+++ src/components/Opinions/OpinionForm.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
+import OpinionButton from './OpinionButton';
 
-function OpinionForm() {
-    return (
-        <form className="opinion-form">
-            <p>form</p>
-        </form>
-    )
+const OpinionForm = () => {
+
+        const [address, setAddress] = useState('');
+        const [opinion, setOpinion] = useState('');
+      
+        const handleSubmit = (event) => {
+          event.preventDefault();
+          // Tutaj możesz dodać logikę obsługi przesłania formularza
+          console.log('Adres:', address);
+          console.log('Opinia:', opinion);
+        };
+      
+        return (
+          <form className='opinion-form' onSubmit={handleSubmit}>
+            <label className='label-opinion'>
+              Toilet address:
+              <input className='adress-input'
+                type="text"
+                value={address}
+                onChange={(event) => setAddress(event.target.value)}
+              />
+            </label>
+            <br />
+            <label className='label-opinion'>
+              Your opinion:
+              <textarea className='opinion-input'
+                value={opinion}
+                onChange={(event) => setOpinion(event.target.value)}
+              />
+            </label>
+            <br />
+          <OpinionButton />
+          </form>
+        );
 }
 
 export default OpinionForm;
